4831ce414867e5d13b3ad7bc26c826445d58de3a
Nines89 <50660601+Nines89@users.noreply.github.com>
2022-05-24 22:00:00 +0200

    toast_example
    load a folder kivy example

---
+++ CoseVarieApps/toast/toast.py
@@ -0,0 +1,50 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.toast import toast
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.tooltip import MDTooltip
+
+KV = '''
+BoxLayout:
+    orientation:'vertical'
+
+    MDToolbar:
+        id: toolbar
+        title: 'Test Toast'
+        md_bg_color: app.theme_cls.primary_color
+        left_action_items: [['menu', lambda x: '']]
+
+    FloatLayout:
+        
+        TooltipMDIconButton:
+            icon: 'dog-side'
+            tooltip_text:'icon'
+            pos_hint: {'center_x': .5, 'center_y': .8}
+
+        TooltipMDRaisedButton:
+            text: 'TEST KIVY TOAST'
+            tooltip_text:'button'
+            on_release: app.show_toast()
+            pos_hint: {'center_x': .5, 'center_y': .5}
+
+<TooltipMDRaisedButton@MDRaisedButton+MDTooltip>:
+
+'''
+
+
+class TooltipMDIconButton(MDIconButton, MDTooltip):
+    pass
+
+
+class Test(MDApp):
+    def show_toast(self):
+        '''Displays a toast on the screen.'''
+
+        toast('Test Kivy Toast')
+
+    def build(self):
+        return Builder.load_string(KV)
+
+
+if __name__ == "__main__":
+    Test().run()
